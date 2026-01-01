05e4b47ef415fe664cbb4d7f860dd2181bad64e5
Dayanand Suryawanshi <dayanandsuryawanshi0207@gmail.com>
2026-01-01 21:32:57 +0530

    updated cors and api settings

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import "./config/passport.js"
 const app=express();
 const port=process.env.PORT || 5000;
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-voice-assistant-a1fn.onrender.com",
     credentials: true
 }));
 
